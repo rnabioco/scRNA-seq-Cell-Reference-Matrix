f884609d03191e96672b58d5182fc0fa874af49a
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-07-17 14:04:23 -0600

    Added scripts to generate ref matrices

---
+++ R/musMusculus/GSE119228/GSE119228AB893.r
@@ -0,0 +1,41 @@
+library(dplyr)
+library(Seurat)
+library(patchwork)
+library(clustifyr)
+library(tidyverse)
+library(digest)
+library(here)
+
+proj_dir <- here()
+GSE119228 <- file.path(proj_dir, "Reference-Matrix-Generation", "data", "GSE119228_RAW", "GSM3361719_AB894.txt.gz")
+
+mat_LungDevelopment <- read.csv(GSE119228)
+mat_LungDevelopment <- mat_LungDevelopment %>%
+  # as.data.frame() %>%
+  #column_to_rownames('X')
+#  as.matrix() %>%
+#  t()
+mat_LungDevelopment[1:5, 1:5]
+
+meta_LungDevelopment <- read_tsv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE119nnn/GSE119228/suppl/GSE119228_metadata.txt.gz")
+meta_LungDevelopment <- meta_LungDevelopment %>% column_to_rownames("X1") %>% t() %>% as.data.frame()
+meta2 <- meta_LungDevelopment[colnames(mat_LungDevelopment),]
+meta_LungDevelopment
+sum(colnames(mat_LungDevelopment) %in% meta_LungDevelopment$cluster)
+ncol(mat_LungDevelopment)
+
+source("~/Reference-Matrix-Generation/R/utils/utils.r")
+checkRawCounts(as.matrix(mat_LungDevelopment))
+
+GSE119228Normalized <- NormalizeData(mat_LungDevelopment)
+
+new_ref_matrix <- average_clusters(mat = GSE119228Normalized, metadata = meta2, cluster_col = "cluster_annotation", if_log = FALSE)
+new_ref_matrix_hashed <- average_clusters(mat = mat_LungDevelopment, metadata = meta_LungDevelopment, if_log = FALSE)
+head(new_ref_matrix)
+tail(new_ref_matrix)
+newcols <- sapply(colnames(new_ref_matrix_hashed), digest, algo = "sha1")
+colnames(new_ref_matrix_hashed) <- newcols
+head(new_ref_matrix_hashed)
+tail(new_ref_matrix_hashed)
+saveRDS(new_ref_matrix_hashed, "GSE131957Hashed.rds")
+saveRDS(new_ref_matrix, "GSE131957.rds")
