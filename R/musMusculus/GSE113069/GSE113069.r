6d82c6ad837571ecb4356dd374ef9fea2b852de3
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-07-20 11:42:11 -0600

    Fixed wa warning based on Rui's review

--- R/musMusculus/GSE113069/GSE113069.r
+++ R/musMusculus/GSE113069/GSE113069.r
@@ -15,7 +15,9 @@ mat_subregions <- mat_subregions %>%
 mat_subregions[1:5, 1:5]
 
 meta_subregions <- read_tsv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE113nnn/GSE113069/suppl/GSE113069_metadata.txt.gz")
-meta_subregions
+meta_subregions <- meta_subregions %>% mutate(id = str_remove(str_c(id, file, sep = "__"), "\\.dat")) %>% column_to_rownames("id")
+meta_subregions2 <- meta_subregions[colnames(mat_subregions),]
+meta_subregions2
 ncol(mat_subregions)
 
 source("~/Reference-Matrix-Generation/R/utils/utils.r")
@@ -23,7 +25,7 @@ checkRawCounts(as.matrix(mat_subregions))
 
 GSE113069Normalized <- NormalizeData(mat_subregions)
 
-new_ref_matrix <- average_clusters(mat = GSE113069Normalized, metadata = meta_subregions$note2, if_log = FALSE)
+new_ref_matrix <- average_clusters(mat = GSE113069Normalized, metadata = meta_subregions2$note2, if_log = FALSE)
 head(new_ref_matrix)
 tail(new_ref_matrix)
 saveRDS(new_ref_matrix, "GSE113069.rds")
