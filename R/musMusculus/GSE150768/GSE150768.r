ddb994f6952fd78e0af7d97dacce2a8016c4b38d
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-07-20 13:19:05 -0600

    Fixed r script

--- R/musMusculus/GSE150768/GSE150768.r
+++ R/musMusculus/GSE150768/GSE150768.r
@@ -6,24 +6,24 @@ library(tidyverse)
 library(digest)
 library(here)
 
-mat_atheroscleroticlesions <- read.table("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE150nnn/GSE150768/suppl/GSE150768_scRNAseq_rawmatrix.txt.gz", sep = "\t", skipNul = 3)
-mat_atheroscleroticlesions <- mat_atheroscleroticlesions %>%
-  # as.data.frame() %>%
-  column_to_rownames('B11__d1')
-#  as.matrix() %>%
-#  t()
+proj_dir <- here()
+GSE150768 <- file.path(proj_dir, "Reference-Matrix-Generation", "data", "GSE150768", "GSE150768_scRNAseq_rawmatrix.txt")
+GSE150768Meta <- file.path(proj_dir, "Reference-Matrix-Generation", "data", "GSE150768", "GSE150768_scRNAseq_metadata.txt")
+
+mat_atheroscleroticlesions <- read.table(GSE150768, sep = " ")
+mat_atheroscleroticlesions <- as.matrix(mat_atheroscleroticlesions)
 mat_atheroscleroticlesions[1:5, 1:5]
 
-meta_atheroscleroticlesions <- read_tsv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE113nnn/GSE113069/suppl/GSE113069_metadata.txt.gz")
+meta_atheroscleroticlesions <- read.table(GSE150768Meta, sep = " ")
 meta_atheroscleroticlesions
 ncol(mat_atheroscleroticlesions)
 
 source("~/Reference-Matrix-Generation/R/utils/utils.r")
 checkRawCounts(as.matrix(mat_atheroscleroticlesions))
 
-GSE113069Normalized <- NormalizeData(mat_atheroscleroticlesions)
+GSE150768Normalized <- NormalizeData(mat_atheroscleroticlesions)
 
-new_ref_matrix <- average_clusters(mat = GSE113069Normalized, metadata = meta_atheroscleroticlesions, if_log = FALSE)
+new_ref_matrix <- average_clusters(mat = GSE150768Normalized, metadata = meta_atheroscleroticlesions$celltype, if_log = FALSE)
 head(new_ref_matrix)
 tail(new_ref_matrix)
 saveRDS(new_ref_matrix, "GSE150768.rds")
